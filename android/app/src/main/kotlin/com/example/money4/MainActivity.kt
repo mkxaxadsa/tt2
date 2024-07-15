70e065dd0f31cc59c1bcccd65e0743e1b4393b94
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-07-15 13:12:33 +0200

    fix

--- android/app/src/main/kotlin/com/example/money4/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tanwy.kizmaz.io
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/features/home/bloc/home_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_bloc.dart';
-
-abstract class HomeEvent {}
-
-class ChangePageEvent extends HomeEvent {
-  final int index;
-  ChangePageEvent({required this.index});
-}
